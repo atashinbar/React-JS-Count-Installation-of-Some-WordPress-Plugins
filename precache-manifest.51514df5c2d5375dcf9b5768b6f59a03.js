561758ee9492ccb64b5d6e7c449774ed264d8a7b
atashinbar <ali.atashinbar@gmail.com>
2019-09-20 19:02:40 +0430

    Updates

---
+++ precache-manifest.51514df5c2d5375dcf9b5768b6f59a03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8504c522ee69631ca9fe320f05ebbeae",
+    "url": "/stat/index.html"
+  },
+  {
+    "revision": "204c8f37ffa2a4b38ad9",
+    "url": "/stat/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5a9457043311673d4abc",
+    "url": "/stat/static/js/2.1586f963.chunk.js"
+  },
+  {
+    "revision": "204c8f37ffa2a4b38ad9",
+    "url": "/stat/static/js/main.493dbc78.chunk.js"
+  },
+  {
+    "revision": "f206fbaa3eebc77da744",
+    "url": "/stat/static/js/runtime-main.a399047a.js"
+  }
+]);
